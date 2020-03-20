900f36338257dbf0d7d227546c9a2cf91c376485
James Stewart <jamestewartjr1@gmail.com>
2020-03-20 02:07:53 -0400

    Add Headder componet

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,23 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import {FaToggleOn, FaListUl, FaPlus} from 'react-icons/fa'
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 export const Header = () => {
+  const classes = useStyles();
+
   return (
-    <header 
-      className="header"
-      data-testid="header"
-    >
-      <nav>
-        <div className="logo">
-          <FaListUl/>
-        </div>
-        <div className="settings">
-          <ul>
+    <header className={classes.root} data-testid="header">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <FaListUl className="logo"/>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Notices
+          </Typography>
+          <Button color="inherit">Login</Button>
+          {/* <ul>
             <li data-testid="add-task-action" className="settings__add"><FaPlus/></li>
             <li data-testid="dark-mode-action" className="settings__darkmode"><FaToggleOn/></li>
-          </ul>
-        </div>
-      </nav>
+          </ul> */}
+        </Toolbar>
+      </AppBar>
     </header>
-  )
+  );
 }
+
+Header.propTypes = {
+  container: PropTypes.any,
+};
+
+export default Header;
