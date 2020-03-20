151efedc02d7344d350e852dacc032af34ff9a6e
James Stewart <jamestewartjr1@gmail.com>
2020-03-20 02:13:04 -0400

    Add body layout structure

---
+++ src/components/layout/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+export const Content = () => {
+  return (
+    <React.Fragment>
+      <Container fluid>
+        <Typography component="div" />
+      </Container>
+    </React.Fragment>
+  );
+}
