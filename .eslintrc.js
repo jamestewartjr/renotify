4a8d97baac2a7a7981136efbebc5f8d3c11cd914
James Stewart <jamestewartjr1@gmail.com>
2020-03-21 22:59:26 -0400

    Add lint rules and package script

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": [
+    "react-app",
+    "plugin:jsx-a11y/recommended"
+  ],
+  "rules" : {
+      "indent": ["error", 2] // A custom style-related rule for example
+      // More custom rules here
+  }
+}
