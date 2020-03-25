b6c7c51abb37fe0bdb59566c24ae3d214e640d8b
James Stewart <jamestewartjr1@gmail.com>
2020-03-25 06:22:53 -0400

    BCreate api with firestore

--- server/index.js
+++ server/index.js
@@ -7,15 +7,16 @@ admin.initializeApp({
   databaseURL: "https://renotify-cfa59.firebaseio.com"
 });
             
-const db = admin.database();
+// const db = admin.database(); //RTD connection
+const db = admin.firestore(); //Firestore connection
 
 const typeDefs  = graphql`
   type Notices{
     id: ID
     name: String
-    sender: String
-    sourceName: String
-    receivedTime: String
+    user: String
+    platformId: String
+    createdAt: String
   
   }
   type Query{
@@ -24,20 +25,27 @@ const typeDefs  = graphql`
 `
 
 const resolvers = {
+  // Query: {  //Query for RTD read
+  //   async fetchAllNotices() {
+  //     let ref = db.ref("notice");
+  //     try{
+  //       const notices = await ref.once("value")
+  //         .then(snapshot => snapshot.val()) 
+  //         .then(val => Object.keys(val)
+  //           .map(key => val[key]))
+  //       return notices;
+  //     }
+  //     catch(error){
+  //       throw new Error(error)
+  //     }
+  //   }
+  // }
   Query: {
-    async fetchAllNotices() {
-      let ref = db.ref("notice");
-      try{
-        const notices = await ref.once("value")
-          .then(snapshot => snapshot.val()) 
-          .then(val => Object.keys(val)
-            .map(key => val[key]))
-        console.log('notices: ', notices)
-        return notices;
-      }
-      catch(error){
-        throw new Error(error)
-      }
+    async fetchAllNotices() { //Query for Firestore read
+      const notices = await db
+        .collection('notices')
+        .get();
+      return notices.docs.map(notice => notice.data());
     }
   }
 }
