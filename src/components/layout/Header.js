63623b55fd5efecd0825aa0c6d0f1761df5af8c9
James Stewart <jamestewartjr1@gmail.com>
2020-03-28 02:42:08 -0400

    wip for navigation drawer

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,24 +6,56 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import {MdMenu} from 'react-icons/md'
 import { useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 export const Header = () => {
   const classes = useTheme();
 
+  const [state, setState] = useState({
+    open: false,
+  });
+
+  const toggleDrawer = () => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, open:true });
+  };
+
   return (
     <AppBar position="static" data-testid="header" color="primary" >
       <Toolbar>
-        <IconButton edge="start" className="logo" color="inherit" aria-label="menu">
-          <MdMenu className={classes.menuButton} />
+        <IconButton 
+          className={classes.menuButton} 
+          onClick={toggleDrawer()} 
+          aria-label="menu"
+        >
+          <Drawer anchor="left" open={state.open} onClose={toggleDrawer()}>
+            <div
+              className={classes.list}
+              role="presentation"
+              onClick={toggleDrawer()}
+              onKeyDown={toggleDrawer()}
+            >
+              <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemIcon><MdMenu /></ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          </Drawer>
+          <MdMenu />
         </IconButton>
-        <Typography variant="h6" className={classes.title} align='center'>
+        <Typography variant="h6" className={classes.title}>
           renotify
         </Typography>
-        {/* <Button color="inherit">Login</Button> */}
-        {/* <ul>
-          <li data-testid="add-task-action" className="settings__add"><FaPlus/></li>
-          <li data-testid="dark-mode-action" className="settings__darkmode"><FaToggleOn/></li>
-        </ul> */}
       </Toolbar>
     </AppBar>
   );
