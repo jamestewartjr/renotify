7161022d608acead3687d9eb9ae341d556d16fc5
James Stewart <jamestewartjr1@gmail.com>
2020-04-05 22:45:34 -0400

    Update imports

--- src/components/SignupCTA.js
+++ src/components/SignupCTA.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/button';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Link as RouterLink } from 'react-router-dom';
 
