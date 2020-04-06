f68abbfee41c0554724afd041d8436a5c71521df
James Stewart <jamestewartjr1@gmail.com>
2020-04-05 21:48:18 -0400

    wip for homepage

---
+++ src/components/SignupCTA.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  hero: {
+    padding: theme.spacing(12, 4, 12),
+    backgroundColor: theme.palette.background.primary,
+    borderRadius: '5%',
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+export default function Signup()  {
+  const classes = useStyles();
+  return (
+    <Container maxWidth="sm" component="main" className={classes.heroContent}>
+      <Card>
+        <Typography variant="h5" align="left" color="textPrimary" component="p">
+          Sign up now!
+        </Typography>
+        <button className="btn btn-secondary mt-2 mb-2" type="submit" value="Submit"> Signup</button>
+      </Card>
+    </Container>
+  )}
+
+Signup.defaultProps = {
+  location: '',
+};
+
+Signup.propTypes = {
+  location: PropTypes.string,
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,171 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {FaRegStar} from 'react-icons/fa'
+import Signup from '../components/SignupCTA'
 
-const Home = () => {
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  hero: {
+    padding: theme.spacing(12, 4, 12),
+    // backgroundColor: theme.palette.background.primary,
+    borderRadius: '5%',
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+const tiers = [
+  {
+    title: 'Free',
+    price: '0',
+    description: ['1 user', '1000 notifications', 'Email support'],
+    buttonText: 'Sign up for free',
+    buttonVariant: 'outlined',
+  },
+  // {
+  //   title: 'Pro',
+  //   subheader: 'Most popular',
+  //   price: '15',
+  //   description: [
+  //     '20 users included',
+  //     '10 GB of storage',
+  //     'Help center access',
+  //     'Priority email support',
+  //   ],
+  //   buttonText: 'Get started',
+  //   buttonVariant: 'contained',
+  // },
+  // {
+  //   title: 'Enterprise',
+  //   price: '30',
+  //   description: [
+  //     '50 users included',
+  //     '30 GB of storage',
+  //     'Help center access',
+  //     'Phone & email support',
+  //   ],
+  //   buttonText: 'Contact us',
+  //   buttonVariant: 'outlined',
+  // },
+];
+
+export default function Pricing() {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
-      <Typography component="div" />
-      <div>
-        Home
-      </div>
+      <CssBaseline />
+      {/* Hero unit */}
+      <Container maxWidth="" component="main" className={classes.hero}>
+        <Typography component="h1" variant="h2" align="center" color="#fff">
+          Tired of Notifications?
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" component="p">
+          Manage your notifications by deciding when you want to see them with Renotify.
+        </Typography>
+      </Container>
+
+      <Signup/>
+
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+          Pricing
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" component="p">
+          Quickly build an effective pricing table for your potential customers with this layout.
+          It&apos;s built with default Material-UI components with little customization.
+        </Typography>
+      </Container>
+      {/* End hero unit  */}
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          {tiers.map((tier) => (
+            // Enterprise card is full width at sm breakpoint
+            <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
+              <Card>
+                <CardHeader
+                  title={tier.title}
+                  subheader={tier.subheader}
+                  titleTypographyProps={{ align: 'center' }}
+                  subheaderTypographyProps={{ align: 'center' }}
+                  action={tier.title === 'Pro' ? <FaRegStar /> : null}
+                  className={classes.cardHeader}
+                />
+                <CardContent>
+                  <div className={classes.cardPricing}>
+                    <Typography component="h2" variant="h3" color="textPrimary">
+                      ${tier.price}
+                    </Typography>
+                    <Typography variant="h6" color="textSecondary">
+                      /mo
+                    </Typography>
+                  </div>
+                  <ul>
+                    {tier.description.map((line) => (
+                      <Typography component="li" variant="subtitle1" align="center" key={line}>
+                        {line}
+                      </Typography>
+                    ))}
+                  </ul>
+                </CardContent>
+                <CardActions>
+                  <Button fullWidth variant={tier.buttonVariant} color="primary">
+                    {tier.buttonText}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </React.Fragment>
   );
 }
-
-export default Home;
