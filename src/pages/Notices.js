d4b7a453b9f7edf8a2c552effcec594200a886dc
James Stewart <jamestewartjr1@gmail.com>
2020-04-11 01:04:43 -0400

    Clean up Notices page

--- src/pages/Notices.js
+++ src/pages/Notices.js
@@ -1,23 +1,19 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { NoticesList } from '../components/NoticesList';
-import {AuthContext} from '../context/auth'
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import NoticesForm from '../components/NoticesForm'
 
-const Notices = props => {
-  const {user} = useContext(AuthContext)
+const Notices = props => (
+  <Container className="notices">
+    <Typography component="div" />
+    <Grid item>
+      <NoticesForm />
+      <NoticesList />
+    </Grid>
+  </Container>
+)
 
-  return(
-    <Container component="notices">
-      <Typography component="div" />
-      <Grid items alignItems="stretch">
-        {user && <NoticesForm />}
-        <NoticesList />
-      </Grid>
-    </Container>
-  )
-}
 
 export default Notices;
