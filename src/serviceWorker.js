01b302431411dd12f2c1744c3e4db70bb5681206
James Stewart <jamestewartjr1@gmail.com>
2020-04-11 21:35:07 -0400

    Clean s-worker code

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,15 +1,3 @@
-// This optional code is used to register a service worker.
-// register() is not called by default.
-
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
-
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read https://bit.ly/CRA-PWA
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -40,12 +28,12 @@ export function register(config) {
 
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
-          );
-        });
+        // navigator.serviceWorker.ready.then(() => {
+        //   console.log(
+        //     'This web app is being served cache-first by a service ' +
+        //       'worker. To learn more, visit https://bit.ly/CRA-PWA'
+        //   );
+        // });
       } else {
         // Is not localhost. Just register service worker
         registerValidSW(swUrl, config);
