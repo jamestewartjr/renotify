827245f89066fb7cf73e06d1e66089a224e77463
James Stewart <jamestewartjr1@gmail.com>
2020-04-15 23:50:38 -0400

    Initial service worker add to home prompt

---
+++ src/components/AddToHome.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+
+const AddToHome = () => {
+  const [prompt, setPrompt] = useState(false);
+
+  useEffect(() => {
+    // this will catch the beforeinstallprompt and prevents the native prompt from appearing
+    window.addEventListener('beforeinstallprompt', e => {
+      e.preventDefault();
+      setPrompt(e);
+    });
+  }, []);
+
+  if (!prompt) {
+    return '';
+  }
+
+  return (
+    <button
+      className="text-white bg-indigo-800 hover:bg-indigo-700 rounded-full shadow AddToHome"
+      onClick={() => prompt.prompt() /* fire the prompt on button click */}
+    >
+      <svg viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default AddToHome;
